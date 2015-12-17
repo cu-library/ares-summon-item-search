b6982000f770d348df99b91bdf7b35054df664c6
Kevin Bowrin <kevin_bowrin@carleton.ca>
2015-12-17 13:45:50 -0500

    Name Change

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Evocation
+# ares_summon_item_search
 
 Using Summon search results to prepopulate forms in Ares.
 
@@ -7,9 +7,9 @@ Using Summon search results to prepopulate forms in Ares.
 If Ares' webroot is at `C:\inetpub\wwwroot\ares`, then you can use `movefiles.cmd` to move js and css files to the correct locations. 
 To do so, right click on `movefiles.cmd` and select "Run as administrator..." 
 
-You can also copy the two files manually. Copy `evocator.js` to the js folder. Copy `evocator.css` to the css folder.
+You can also copy the two files manually. Copy `ares_summon_item_search.js` to the js folder. Copy `ares_summon_item_search.css` to the css folder.
 
 Add this to the head of IRFArticle.html:
 ```html
-<script language="javascript" src="js/evocator.js"></script>
+<script language="javascript" src="js/ares_summon_item_search.js"></script>
 ```
--- movefiles.cmd
+++ movefiles.cmd
@@ -1,22 +1,22 @@
 @ECHO OFF
 SETLOCAL
 
-REM This files moves evocation.js and evocation.css to the Ares Webroot. 
+REM This files moves ares_summon_item_search.js and ares_summon_item_search.css to the Ares Webroot. 
 
 REM Change to the same directory as the current script
 @setlocal enableextensions
 @cd /d "%~dp0"
 
 SET installdir=C:\inetpub\wwwroot\ares
-SET jsinstalllocation=%installdir%\js\evocation.js
-SET cssinstalllocation=%installdir%\css\evocation.css 
+SET jsinstalllocation=%installdir%\js\ares_summon_item_search.js
+SET cssinstalllocation=%installdir%\css\ares_summon_item_search.css 
 
 ECHO To use this script, right click and select "Run as administrator" option.
-ECHO You will still need to include evocator.js in item forms. See the README.
+ECHO You will still need to include ares_summon_item_search.js in item forms. See the README.
 
 ECHO Installing...
-COPY /V /-Y evocation.js %jsinstalllocation%
-COPY /V /-Y evocation.css %cssinstalllocation%
+COPY /V /-Y ares_summon_item_search.js %jsinstalllocation%
+COPY /V /-Y ares_summon_item_search.css %cssinstalllocation%
 ECHO Done!
 PAUSE
 

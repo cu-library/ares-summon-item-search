f9ae34f8187f1bec102e52f6124f9ba6b320d19b
Kevin Bowrin <kevin_bowrin@carleton.ca>
2015-12-17 13:42:30 -0500

    changed names

--- movefiles.cmd
+++ movefiles.cmd
@@ -1,22 +1,22 @@
 @ECHO OFF
 SETLOCAL
 
-REM This files moves evocator.js and evocator.css to the Ares Webroot. 
+REM This files moves evocation.js and evocation.css to the Ares Webroot. 
 
 REM Change to the same directory as the current script
 @setlocal enableextensions
 @cd /d "%~dp0"
 
 SET installdir=C:\inetpub\wwwroot\ares
-SET jsinstalllocation=%installdir%\js\evocator.js
-SET cssinstalllocation=%installdir%\css\evocator.css
+SET jsinstalllocation=%installdir%\js\evocation.js
+SET cssinstalllocation=%installdir%\css\evocation.css 
 
 ECHO To use this script, right click and select "Run as administrator" option.
 ECHO You will still need to include evocator.js in item forms. See the README.
 
 ECHO Installing...
-COPY /V /-Y evocator.js %jsinstalllocation%
-COPY /V /-Y evocator.css %cssinstalllocation%
+COPY /V /-Y evocation.js %jsinstalllocation%
+COPY /V /-Y evocation.css %cssinstalllocation%
 ECHO Done!
 PAUSE
 

85cbcecdbfdd53bab0cbe3baa30346f762a4d9a0
Kevin Bowrin <kevin_bowrin@carleton.ca>
2015-12-17 12:28:47 -0500

    Created an short and sweet install script.

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # Evocator
 
 Summon+Ares
+
+## Installation
+
+Use `movefiles.cmd` to move the javascript and css files to the Ares webroot. You can also do this manually. 
+
+Add this to the head of IRFArticle.html:
+```html
+<script language="javascript" src="js/evocator.js"></script>
+```
---
+++ movefiles.cmd
@@ -0,0 +1,25 @@
+@ECHO OFF
+SETLOCAL
+
+REM This files moves evocator.js and evocator.css to the Ares Webroot. 
+
+REM Change to the same directory as the current script
+@setlocal enableextensions
+@cd /d "%~dp0"
+
+SET installdir=C:\inetpub\wwwroot\ares
+SET jsinstalllocation=%installdir%\js\evocator.js
+SET cssinstalllocation=%installdir%\css\evocator.css
+
+ECHO To use this script, right click and select "Run as administrator" option.
+ECHO You will still need to include evocator.js in item forms. See the README.
+
+ECHO Installing...
+COPY /V evocator.js %jsinstalllocation%
+COPY /V evocator.css %cssinstalllocation%
+ECHO Done!
+PAUSE
+
+
+
+

1e1b5f9d816ee1e9e55d7e06bbf4d65e890ab0bc
Kevin Bowrin <kevin_bowrin@carleton.ca>
2016-01-08 13:53:23 -0500

    Integrate jQuery UI images and first AJAX calls.

--- README.md
+++ README.md
@@ -4,19 +4,17 @@ Using Summon search results to prepopulate forms in Ares.
 
 ## Installation
 
-If Ares' webroot is at `C:\inetpub\wwwroot\ares`, then you can use `movefiles.cmd` to move js and css files to the correct locations. 
+If Ares' webroot is at `C:\inetpub\wwwroot\ares`, then you can use `movefiles.cmd` to move js, css, and images files to the correct locations. 
 To do so, right click on `movefiles.cmd` and select "Run as administrator..." 
 
-You can also copy the three files manually. Copy `ares-summon-item-search.js`, `jquery-ui-1.11.4.min.js`, and `jquery-1.11.3.min.js` to the js folder. 
-Copy `ares-summon-item-search.css` and `jquery-ui-1.11.4.min.css` to the css folder.
+You can also copy the three files manually. Copy `ares-summon-item-search.js`, `jquery-ui-1.11.4.min.js`, and `jquery-1.11.3.min.js` to the js directory. 
+Copy `ares-summon-item-search.css`, `jquery-ui-1.11.4.min.css`, and the `jquery-ui-1.11.4-images` directory to the css directory.
 
 Add this to the bottom of the `<body>` tag:
 ```html
-<!-- jQuery include from CDN, fallback if fail. Noconflict, then ares-summon-item-search -->
-<script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
-<script>window.jQuery || document.write('<script src="js/jquery-1.11.3.min.js"><\/script>')</script>
-<script src="//code.jquery.com/ui/1.11.4/jquery-ui.min.js"></script>
-<script>window.jQuery.ui || document.write('<script src="js/jquery-ui-1.11.4.min.js><\/script>')</script>
+<!-- jQuery include from CDN, fallback if fail. Noconflict, then ares_summon_item_search -->
+<script src="js/jquery-1.11.3.min.js"></script>
+<script src="js/jquery-ui-1.11.4.min.js"></script>
 <script>AresSummonItemSearchjQuery = jQuery.noConflict(true);</script>
 <script src="js/ares-summon-item-search.js"></script>
 ```
--- ares-summon-item-search.js
+++ ares-summon-item-search.js
@@ -3,14 +3,59 @@
 var AresSummonItemSearch = AresSummonItemSearch || {};
 
 /* Init function */
-AresSummonItemSearch.init = function(jq){
+AresSummonItemSearch.init = function($, loricaURL, summonURL){
+
+  var parent = AresSummonItemSearch;
+ 
+  parent.$ = $;
+  parent.loricaURL = loricaURL;
+  parent.summonURL = summonURL;
+	
   // Add the css file links.  
-  jq('head').append('<link rel="stylesheet" href="css/ares-summon-item-search.css" type="text/css" />');
-  jq('head').append('<link rel="stylesheet" href="css/jquery-ui-1.11.4.min.css" type="text/css" />');
+  $('head').append('<link rel="stylesheet" href="css/ares-summon-item-search.css" type="text/css" />');
+  $('head').append('<link rel="stylesheet" href="css/jquery-ui-1.11.4.min-edit.css" type="text/css" />');
+  
+  //Health check - Ensure Is Summon is available
+  $.ajax({
+    url: loricaURL,
+	data: { "accept": "application/json",
+            "path": "/2.0.0/search/health" }
+  })
+  .done(function(data) { 
+    console.log(data);  
+    // Lorica has returned a header we can use. 
+    $.ajax({
+      url: summonURL+"/health",
+	  headers: { "Accept": "application/json",
+	             "x-summon-date": data.timestamprfc2616,
+				 "Authorization": data.authorizationheader}
+    })
+	.done(function(data) {
+	  console.log(data);
+	});     
+  });  
+  
+  //Find out if we're on the right page by Action and Form query params  
+  var query = window.location.search.substring(1);
+  if ( ~query.indexOf('Action=10') && ~query.indexOf('Form=21') ) {
+    // Journal Article (Electronic)
+    $( "#Title" ).autocomplete({
+	    source: this.SearchJournalTitle,
+		minLength: 4
+    });
+  }
 }
 
-/* When the DOM is ready, do work! */
-AresSummonItemSearchjQuery(document).ready(function(jq){
-  AresSummonItemSearch.init(jq);
-});
+
+/* Autocomplete Source - Search Journal Title */
+AresSummonItemSearch.SearchJournalTitle = function (request, response) {
+  var parent = AresSummonItemSearch;
+  
+  console.log(request);
+  console.log(parent);
+  
+  response([ "c++", "java", "php", "coldfusion", "javascript", "asp", "ruby" ]);
+}
+
+
 
